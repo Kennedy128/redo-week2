0b00a2c99d61294c71fa7dbcee1493043c93246a
Santa claus <santa@northpole.com>
2020-02-24 20:52:08 +0300

    adding arrays queries

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,43 @@
-function myfunction() {
-    var year = document.getElementById("yearborn").value;
-    var ccentury = parseInt(year.slice(0, 2));
-    var yyear = parseInt(year.slice(2, 4));
-    var mmonth = parseint(document.getElementById("monthborn").value);
-    if (mmonth <= 0 || mmonth > 12) {
-        alert("the month entered does not apply,must be between 1 and 12");
+function akanBirth(){
+
+    let year = document.getElementById("birthyear").value;
+    let CCentury = parseInt(year.slice(0,2));
+    let YYear = parseInt(year.slice(2,4));
+    let MMonth = parseInt(document.getElementById("birthmonth").value) ;
+    let DDay = parseInt(document.getElementById("birthdate").value);
+
+    if (DDay <= 0 || DDay>31){
+        document.getElementById('birthddate').value = '';
+        alert("invalid input,\nDates must be between 1 and 31 ");
+        return;   
     }
-    var dday = parseInt(document.getElementById("dayborn").value);
-    if (dday <= 0 || dday > 31) {
-        alert("the date entered does not apply,must be between 1 and 31");
+    if (MMonth <= 0 || MMonth>12){
+        document.getElementById('birthmonth').value = '';
+        alert("invalid month,\nMonths must be between 1 and 12 ");
+        return;   
+    }
+    if(MMonth === ""||DDay===""||year===""){
+        alert("make sure to fill in all the blank spaces.");
+        return;
+    }
+
+    let gender = document.getElementById("gender").value;
+    let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let male=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    let female=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    
+    let dayOfTheWeek=parseInt((( ( (CCentury/4) -2*CCentury-1) + ((5*YYear/4) ) + ((26*(MMonth+1)/10)) + DDay )%7));
+    if (gender === "female"){
+
+        document.getElementById("dayborn").innerHTML=("Your birth day is "+days[dayOfTheWeek].toUpperCase());
+        document.getElementById("name").innerHTML=("Your Akan name is "+ female[dayOfTheWeek]+" ");
 
     }
-    var gender = document.getElementById("gendertype").value;
-
-    var male = ["Kwasi", "Kwaduo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-    var dayOfTheWeek = parseInt((( ( (ccentury / 4) - 2*ccentury - 1) + ((5 * yyear / 4) ) + ((26 * (mmonth + 1)/10)) + dday) % 7));
-
-    if (gender === "female") {
-
-        document.getElementById("dayborn").innerText = days[dayOfTheWeek];
-        document.getElementById("name").innerText = female[dayOfTheWeek];
+    else{
+        document.getElementById("dayborn").innerHTML=("Your birth day is "+days[dayOfTheWeek].toUpperCase());
+        document.getElementById("name").innerHTML=( " Your Akan name is "+ male[dayOfTheWeek]+" ");
     }
-    else {
-        document.getElementById("dayborn").innerText = days[dayOfTheWeek];
-        document.getElementById("name").innerText = male[dayOfTheWeek];
-    }
+    document.getElementById("myForm").reset();
+
 
 }
-
-
-
-
-
-
-
-
-
-
